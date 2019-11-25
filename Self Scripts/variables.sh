317e9898fa232f7927033d0cee3282e639794dd8
adwait-thattey <coderdude1999@gmail.com>
2019-11-25 21:56:58 +0530

    new changes

---
+++ Self Scripts/variables.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+set -a
+
+if [ -d "/run/media" ]; then
+	echo "Using Manjaro prefix"
+	DRIVES_PREFIX="/run/media"
+elif [ -d "/media" ]; then
+	echo "Using Ubuntu Path"
+	DRIVES_PREFIX="/media"
+
+else 
+	echo "DRIVE PREFIX for this OS not found"
+
+fi
+	echo $DRIVES_PREFIX
+
+DRIVE_ADWAIT="$DRIVES_PREFIX/$USER/Adwait"
+
+if [ -d $DRIVE_ADWAIT ]; then
+
+	DIR_STUDY="$DRIVE_ADWAIT/Study"
+	DIR_COLLEGE_STUDY="$DIR_STUDY/College Study"
+	DIR_CURRENT_SEM="$DIR_COLLEGE_STUDY/sem5"
+	DIR_PROJECTS="$DRIVE_ADWAIT/Projects"
+	DIR_SOFTWARES="$DRIVE_ADWAIT/Softwares"
+	DIR_TV_SERIES="$DRIVE_ADWAIT/TVS"
+
+else
+	echo "Drive Adwait Not found"
+fi
+
+DRIVE_SECONDRY="$DRIVES_PREFIX/$USER/Secondry"
+
+if [ -d $DRIVE_SECONDRY ]; then
+	DRIVE_GAMES="$DRIVE_SECONDRY/Games"
+else
+	echo "Drive Secondry Not found"
+
+fi
+
+DRIVE_TOSHIBA="$DRIVES_PREFIX/$USER/TOSHIBA EXT"
+
+set +a
