3fc50fa08357b8f38f46df9bc23c2b37a6ff3bd2
adwait-thattey <coderdude1999@gmail.com>
2020-02-04 19:14:36 +0530

    gpu test module

--- Others/gputest/gfgtest.py
+++ Others/gputest/gfgtest.py
@@ -3,23 +3,26 @@ import numpy as np
 # to measure exec time 
 from timeit import default_timer as timer    
   
+
+n = 100000000                        
 # normal function to run on cpu 
 def func(a):                                 
-    for i in range(10000000): 
-        a[i]+= 1      
+    for i in range(n-2): 
+        a[i]*= a[i+1]      
   
 # function optimized to run on gpu  
 @jit(target ="cuda")                          
 def func2(a): 
-    for i in range(10000000): 
-        a[i]+= 1
+    for i in range(n-2): 
+        [i for i in range(1,n)]
+
+
 if __name__=="__main__": 
-    n = 10000000                            
     a = np.ones(n, dtype = np.float64) 
     b = np.ones(n, dtype = np.float32) 
       
     start = timer() 
-    func(a) 
+    #func(a) 
     print("without GPU:", timer()-start)     
       
     start = timer() 
