cfe4f004f5202d844238331c6cb2ffcba65a23e6
Adwait Thattey <coderdude1999@gmail.com>
2021-06-14 20:23:55 +0530

    reverted commit

--- Self Scripts/ytm manager/artists_liked_songs/get_artists_liked_songs.py
+++
@@ -1,9 +0,0 @@
-from ytmusicapi import YTMusic
-import json
-
-ytmusic = YTMusic('../headers_auth.json')
-any_artist_song_id = '9VcDnWMOBtw'
-
-song_details = ytmusic.get_song(any_artist_song_id)
-
-print(song_details)
